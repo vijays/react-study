24aaf84eba57f1342e02dcaaca8f10b4561b79d0
VijayS <vijays@users.noreply.github.com>
2016-05-24 05:22:04 -0400

    Passing props, event handling

--- app.jsx
+++ app.jsx
@@ -4,11 +4,21 @@ import ReactDOM from 'react-dom';
 import Intro from './components/Intro';
 
 class App extends React.Component {
+	
+	constructor() {
+		super();
+		this.state = {name: ""};
+	}
+	
+	changeName(name) {
+		this.setState({name});
+	}
+	
 	render() {
 		return (
 			<div>
-				Hello World!
-				<Intro name = "Vijay"/>
+				Hello World!<p />
+				<Intro name = {this.state.name} changeName={this.changeName.bind(this)}/>
 			</div>
 		);
 	}
--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
 export default class Intro extends Component {
+    	
+	handleChange(e) {
+		const name = e.target.value;
+		this.props.changeName(name);
+	}
+
     render() {
         return (
             <div>
-                Welcome {this.props.name}!
+                Welcome {this.props.name}!<br />
+                <input onChange={this.handleChange.bind(this)}/>
             </div>
         );
     }
